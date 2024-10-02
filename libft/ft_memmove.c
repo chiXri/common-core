37c115de91806690df3b127218aa2b263d10862a
chiXri <marco_chirnos@hotmail.es>
2024-10-03 00:25:57 +0200

    modificacion en ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,43 +6,31 @@
 /*   By: m.chiri <m.chiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 23:24:57 by m.chiri           #+#    #+#             */
-/*   Updated: 2024/10/02 23:30:43 by m.chiri          ###   ########.fr       */
+/*   Updated: 2024/10/03 00:25:30 by m.chiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "libft.h"
-
-*ft_memmove(void *dest_str, const void *src_str, size_t len)
-{
-    unsigned char *devoist = (unsigned char *)dest_str;
-    const unsigned char *src = (const unsigned char *)src_str;
-
-    // Comprobar si hay punteros nulos o longitud cero
-    if (len == 0 || dest == src)
-    {
-        return dest_str;
-    }
-
-    // Si se solapan, asegurarse de copiar de atrás hacia adelante
+void *ft_memmove (void *dest_str, const void *src_str, size_t len) 
+{ 
+    unsigned char* dest = dest_str;
+    const unsigned char* src = src_str;
+    
     if (dest < src)
     {
-        while (len--)
+        while(len--)
         {
             *dest++ = *src++;
         }
-    }
-    else
+    }else
     {
-        // Copiar de derecha a izquierda
-        dest += len; // Ajustar el puntero de destino
-        src += len;  // Ajustar el puntero de origen
-        while (len--)
+        while(len--)
         {
-            *(--dest) = *(--src);
+            dest[len] = src[len];
         }
     }
 
-    return dest_str;
-}
+    return dest_str; 
 
+}
